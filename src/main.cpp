7d4bed228bbd2aa394bd5943fbb1220f949552d6
yarl1k <yaroslawdyr@gmail.com>
2025-09-18 12:46:46 +0300

    added some tests in main for average and median

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <vector>
 #include "Board.h"
+#include "experiment.h"
 
 using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
-    Board board(20);
+    Board board(15);
     board.print();
-    for(int i; i < 100; i++) {
+    for(int i = 0; i < 100; i++) {
         board();
     }
     cout << endl;
     board.print();
+
+    cout << "test average: ";
+    double average = average_multiplicity(board);
+    cout << average << endl;
+
+    cout << "test median: ";
+    double median = median_multiplicity(board);
+    cout << "\n median: " << median << endl;
     return 0;
 }
