58bab3009d7a67f9f2ae70ed032fdfd82d933653
yarl1k <yaroslawdyr@gmail.com>
2025-09-29 22:23:20 +0300

    Delete visualize directory

--- visualize/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("output2.csv", names=["cell", "count"])
-ax = df.plot(kind="bar", x="cell", y="count", legend=False)
-ax.set_xticks([])  
-ax.set_xlabel("Cell")
-ax.set_ylabel("Hits count")
-plt.show()
